c11933c1c012cbdbe7aa36aa1c8e3f764105c239
Andrew Zirkel <andrewzirkel@gmail.com>
2019-03-08 13:37:55 -0500

    add SkySiteFacilities
    add SkySiteFacilities

---
+++ SkySiteFacilities/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'SKYSITE Facilities and Archive Sync'
+$fileType = 'msi'
+$url = 'https://fa.skysite.com/app/FASynchronization/SSF&ASync-x86.msi'
+$silentArgs = '/quiet'
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
---
+++ SkySiteFacilities/update.ps1
@@ -0,0 +1,30 @@
+$test=$true
+
+import-module au
+
+$myDir=$myinvocation.mycommand.path | split-path
+
+$url = 'https://fa.skysite.com/app/FASynchronization/SSF&ASync-x86.msi'
+$file='SSF&ASync-x86.msi'
+
+function global:au_SearchReplace {
+       @{
+        ".\tools\chocolateyInstall.ps1" = @{
+        }
+    }
+}
+
+function global:au_GetLatest {
+    if ( -not $test ) {Invoke-WebRequest -Uri $url -OutFile $file}
+
+    $version = & .\Get-MSIFileInformation.ps1 -path "$myDir\$file" -Property ProductVersion
+    $version = $([string]$version).Trim()
+
+
+    @{
+        url32 = $url
+        Version = $version
+    }
+}
+
+Update-Package -ChecksumFor none
