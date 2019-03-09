b3b7e19e0940e9d59bc2882c2eacc738193891ad
Zirkel <zirkelad@umasd.org>
2019-03-09 17:52:13 -0500

    add 64 bit and icon

--- SkySiteFacilities/tools/chocolateyInstall.ps1
+++ SkySiteFacilities/tools/chocolateyInstall.ps1
@@ -1,6 +1,15 @@
 ﻿$packageName = 'SKYSITE Facilities and Archive Sync'
 $fileType = 'msi'
-$url = 'https://fa.skysite.com/app/FASynchronization/SSF&ASync-x86.msi'
+$url32 = 'https://fa.skysite.com/app/FASynchronization/SSF&ASync-x86.msi'
+$url64 = 'https://fa.skysite.com/app/FASynchronization/SSF&ASync-x64.msi'
 $silentArgs = '/quiet'
+
+$osBitness = Get-ProcessorBits
+
+if ($osBitness -eq 64) {
+  $url = $url64
+} else {
+  $url = $url32
+}
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url
--- SkySiteFacilities/update.ps1
+++ SkySiteFacilities/update.ps1
@@ -4,7 +4,8 @@ import-module au
 
 $myDir=$myinvocation.mycommand.path | split-path
 
-$url = 'https://fa.skysite.com/app/FASynchronization/SSF&ASync-x86.msi'
+$url32 = 'https://fa.skysite.com/app/FASynchronization/SSF&ASync-x86.msi'
+$url64 = 'https://fa.skysite.com/app/FASynchronization/SSF&ASync-x64.msi'
 $file='SSF&ASync-x86.msi'
 
 function global:au_SearchReplace {
@@ -15,14 +16,15 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    if ( -not $test ) {Invoke-WebRequest -Uri $url -OutFile $file}
+    if ( -not $test ) {Invoke-WebRequest -Uri $url32 -OutFile $file}
 
     $version = & .\Get-MSIFileInformation.ps1 -path "$myDir\$file" -Property ProductVersion
     $version = $([string]$version).Trim()
 
 
     @{
-        url32 = $url
+        url32 = $url32
+        url64 = $url64
         Version = $version
     }
 }
