49bdefeec3e64b41181622e77792a86194264160
Zirkel <zirkelad@umasd.org>
2019-03-18 09:48:30 -0400

    moderator changes
    address changes requested by moderator.

--- SkySiteFacilities/tools/chocolateyInstall.ps1
+++ SkySiteFacilities/tools/chocolateyInstall.ps1
@@ -1,15 +1,37 @@
 ﻿$packageName = 'SKYSITE Facilities and Archive Sync'
-$fileType = 'msi'
-$url32 = 'https://fa.skysite.com/app/FASynchronization/SSF&ASync-x86.msi'
-$url64 = 'https://fa.skysite.com/app/FASynchronization/SSF&ASync-x64.msi'
-$silentArgs = '/quiet'
+$fileType = 'exe'
+$url32 = 'https://fa.skysite.com/app/FASynchronization/SSF&ASync-x86.exe'
+$url64 = 'https://fa.skysite.com/app/FASynchronization/SSF&ASync-x64.exe'
+$checksum32 = '39af2de8c23b88d3d9419503b2b53952a5153c4aa09cf8c81cbd71db6d47199e'
+$checksum64 = '4f562abe538b1aa0c9437329fcdfa0630364b0d09154adeb34fc6c2096357f0f'
+$checksumType32 = 'sha256'
+$checksumType64 = 'sha256'
+$silentArgs = '/S /v/qn'
+$version='3.4.1.1'
+$validExitCodes=@(0,-1073741819)
 
-$osBitness = Get-ProcessorBits
-
-if ($osBitness -eq 64) {
-  $url = $url64
-} else {
-  $url = $url32
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $fileType
+  url           = $url32
+  url64bit      = $url64
+  silentArgs    = $silentArgs
+  softwareName  = $packageName
+  checksum      = $checksum32
+  checksumType  = $checksumType32
+  checksum64     = $checksum64
+  checksumType64 = $checksumType64
+  validExitCodes = $validExitCodes
 }
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+#This checks to see if current version is already installed
+Write-Output "Checking to see if local install is already up to date..."
+$checkreg64 = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, PSChildName | Where-Object { $_.DisplayName -like 'SKYSITE Facilities and Archive Sync' -and ([Version]$_.DisplayVersion) -eq $version} -ErrorAction SilentlyContinue
+$checkreg32 = Get-ItemProperty HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, PSChildName | Where-Object { $_.DisplayName -like 'SKYSITE Facilities and Archive Sync' -and ([Version]$_.DisplayVersion) -eq $version} -ErrorAction SilentlyContinue
+
+if ($checkreg32 -ne $null -OR $checkreg64 -ne $null) {
+  Write-Output "SSF&A $version is already installed. Skipping download and installation"
+} else {
+  Install-ChocolateyPackage @packageArgs
+}
+
--- SkySiteFacilities/update.ps1
+++ SkySiteFacilities/update.ps1
@@ -1,26 +1,40 @@
-$test=$false
+$test=$true
 
 import-module au
 
 $myDir=$myinvocation.mycommand.path | split-path
 
-$url32 = 'https://fa.skysite.com/app/FASynchronization/SSF&ASync-x86.msi'
-$url64 = 'https://fa.skysite.com/app/FASynchronization/SSF&ASync-x64.msi'
-$file='SSF&ASync-x86.msi'
+$releases = 'https://fa.skysite.com/app/FASynchronization/AutoupdateInfo.xml'
+$url32 = 'https://fa.skysite.com/app/FASynchronization/SSF&ASync-x86.exe'
+$url64 = 'https://fa.skysite.com/app/FASynchronization/SSF&ASync-x64.exe'
+
+
+function global:au_BeforeUpdate {
+  # We need this, otherwise the checksum won't get created
+  # The 64 bit download throws a size error in Get-ChocolateyWebFile.
+  $Latest.ChecksumType32 = 'sha256'
+  $Latest.Checksum32     = Get-RemoteChecksum $Latest.URL32
+  $Latest.ChecksumType64 = 'sha256'
+  $Latest.Checksum64     = Get-RemoteChecksum $Latest.URL64
+
+}
 
 function global:au_SearchReplace {
        @{
         ".\tools\chocolateyInstall.ps1" = @{
+          "(?i)(^\s*\`$checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+          "(?i)(^\s*\`$checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+          "(?i)(^\s*\`$checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+          "(?i)(^\s*\`$checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+           "(?i)(^\s*\`$version\s*=\s*)('.*')" = "`$1'$($Latest.Version)'"
         }
     }
 }
 
 function global:au_GetLatest {
-    if ( -not $test ) {Invoke-WebRequest -Uri $url32 -OutFile $file}
-
-    $version = & .\Get-MSIFileInformation.ps1 -path "$myDir\$file" -Property ProductVersion
-    $version = $([string]$version).Trim()
-
+    $response = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    [xml]$xml = $response.Content
+    $version = $xml.UpdateFileList.LocalFile[0].latestVersion
 
     @{
         url32 = $url32
