9d9865263ce054767015eb41ca8585f5365c91eb
Zirkel <zirkelad@umasd.org>
2019-09-30 13:36:04 -0400

    Add MiteConnect
    Add MiteConnect

---
+++ MitelConnect/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'Mitel Connect'
+$fileType = 'exe'
+$url32 = 'http://upgrade01.sky.shoretel.com/ClientInstall'
+$checksum32 = '4D6BD67D3251C4C4041C6DD7F9B3336B9C6FC85C879B599E1C707289D726DA08'
+$checksumType32 = 'sha256'
+$silentArgs = '/S /v/qn'
+$version='213.100.3953.0'
+$validExitCodes=@(0,-1073741819)
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $fileType
+  url           = $url32
+  silentArgs    = $silentArgs
+  softwareName  = $packageName
+  checksum      = $checksum32
+  checksumType  = $checksumType32
+  validExitCodes = $validExitCodes
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ MitelConnect/update.ps1
@@ -0,0 +1,39 @@
+$test=$true
+
+import-module au
+
+$myDir=$myinvocation.mycommand.path | split-path
+
+$url32 = 'http://upgrade01.sky.shoretel.com/ClientInstall'
+$file= 'MitelConnect.exe'
+
+function global:au_BeforeUpdate {
+  # We need this, otherwise the checksum won't get created
+  # The 64 bit download throws a size error in Get-ChocolateyWebFile.
+  $Latest.ChecksumType32 = 'sha256'
+  $Latest.Checksum32     = checksum -t 'sha256' -f $file
+}
+
+function global:au_SearchReplace {
+       @{
+        ".\tools\chocolateyInstall.ps1" = @{
+          "(?i)(^\s*\`$checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+          "(?i)(^\s*\`$checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+          "(?i)(^\s*\`$version\s*=\s*)('.*')" = "`$1'$($Latest.Version)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    if ( -not $test ) {Invoke-WebRequest -Uri $url32 -OutFile $file}
+
+    $version = (Get-Item "$myDir\$file").VersionInfo.FileVersion
+
+
+    @{
+        url32 = $url32
+        Version = $version
+    }
+}
+
+Update-Package -ChecksumFor none
